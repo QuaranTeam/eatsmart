83d0bbd7073a41ad694172b81a957345c214855d
mickeySTI <imickey216@gmail.com>
2020-03-23 14:48:11 -0400

    Created Spring Boot/MVC project

---
+++ src/main/java/com/eatSmart/eatSmart/EatSmartApplication.java
@@ -0,0 +1,13 @@
+package com.eatSmart.eatSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatSmartApplication.class, args);
+	}
+
+}
