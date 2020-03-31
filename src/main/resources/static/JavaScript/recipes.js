129c8ba8d357f81ac7c2345b31e69dedb6fb19ba
jmalue <jrod2416@gmail.com>
2020-03-31 16:01:44 -0400

    added some js to drop downs

--- src/main/resources/static/JavaScript/recipes.js
+++ src/main/resources/static/JavaScript/recipes.js
@@ -4,11 +4,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
+    var recipeDescription = this.nextElementSibling;
+    if (recipeDescription.style.maxHeight){
+      recipeDescription.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      recipeDescription.style.maxHeight = recipeDescription.scrollHeight + "px";
     } 
   });
 }
