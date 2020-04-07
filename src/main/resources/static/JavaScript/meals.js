ea489b40f087cf74c12b52eabed48924e043496f
kellivalverde <paughka@gmail.comls>
2020-04-07 16:28:33 -0400

    added modals to buttons on Meals Page

--- src/main/resources/static/JavaScript/meals.js
+++ src/main/resources/static/JavaScript/meals.js
@@ -1,12 +1,16 @@
+"use strict"; /* strict mode */
+
+
 /* Get all elements with class="close" */
 var closebtns = document.getElementsByClassName("close");
 var i;
 
 /* Loop through the elements, and hide the parent, when clicked on */
 for (i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", function() {
-  this.parentElement.style.display = 'none';
-})}
+  closebtns[i].addEventListener("click", function () {
+    this.parentElement.style.display = 'none';
+  })
+}
 
 /* Collapsible meal buttons */
 
@@ -14,13 +18,47 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
+
+//should be combined into modal.js file
+function createModal(divId, btnId) {
+
+  // Get the modal
+  var modal = document.getElementById(divId);
+
+  // Get the button that opens the modal
+  var btn = document.getElementById(btnId);
+
+  // Get the <span> element that closes the modal
+  var span = modal.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+createModal("myMealModal", "mealModalButton");
+
+createModal("myGroceryModal", "groceryModalButton");
