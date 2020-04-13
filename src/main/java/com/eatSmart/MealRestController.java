a169eb32f36c93f347780d4fc8c4f782dbb9caca
Dylan Vaca <fredvaca112@icloud.com>
2020-04-13 19:09:32 -0400

    Rest controller for meals and recipes created

--- src/main/java/com/eatSmart/MealRestController.java
+++ src/main/java/com/eatSmart/MealRestController.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -22,7 +23,7 @@ public class MealRestController {
 		return mealRepo.findAll();
 	}
 	
-	@RequestMapping("/{mealName}/recipes")
+	@RequestMapping(value= "/{mealName}/recipes", method = RequestMethod.GET)
 	public Collection<Recipe> findAllRecipesByMeal(@PathVariable(value="mealName") String mealName) {
 		Meal meal = mealRepo.findByNameIgnoreCaseLike(mealName);
 		return recipeRepo.findByMealsContains(meal);
